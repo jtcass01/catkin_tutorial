431f2b8d95ae7d1cfea269dda4cd7abc76cc07f0
Jacob Cassady <jtcass01@louisville.edu>
2018-09-30 19:09:49 -0500

    Tutorial Sections 1-6

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/durzo/Programming/catkin_tutorial/src
+# Build directory: /home/durzo/Programming/catkin_tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/durzo/Programming/catkin_tutorial/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/durzo/Programming/catkin_tutorial/devel;/opt/ros/melodic".split(';') if "/home/durzo/Programming/catkin_tutorial/devel;/opt/ros/melodic" != "" else []
